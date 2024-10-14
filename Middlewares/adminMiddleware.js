b1d6128b273461d40e4cd16979f1374d19e45b29
Sharaf Al Hamza <kingsharaf14@gmail.com>
2024-10-14 14:58:13 +0600

    changes

---
+++ Middlewares/adminMiddleware.js
@@ -0,0 +1,20 @@
+require("doteng").config();
+const jwt = require("jsonwebtoken");
+
+const adminMiddleware = (req, res, next) => {
+  const token = req.headers.token;
+  const decoded = jwt.verify(token, process.env.JWT_ADMIN_PASSWORD);
+
+  if (decoded) {
+    req.userId = decoded.id;
+    next();
+  } else {
+    res.status(403).json({
+      message: "You are not signed in",
+    });
+  }
+};
+
+module.exports = {
+  adminMiddleware: adminMiddleware,
+};
---
+++ routes/admin.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+const { Router } = require("express");
+const { adminModel, courseModel } = require("../database/dbSchema");
+const adminMiddleware = require("../Middlewares/adminMiddleware");
+const adminRouter = Router();
+const jwt = require("jsonwebtoken");
+
+adminRouter.post("/signup", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  await adminModel.create({
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password,
+  });
+
+  res.json({
+    message: "SignUp successful",
+  });
+});
+
+adminRouter.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const admin = await adminModel.findOne({
+    email: email,
+    password: password,
+  });
+
+  if (admin) {
+    const token = jwt.sign(
+      {
+        id: admin._id,
+      },
+      process.env.JWT_ADMIN_PASSWORD
+    );
+
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials",
+    });
+  }
+});
+
+adminRouter.post("/course", adminMiddleware, async (req, res) => {
+  const creatorId = req.userId;
+  const { title, description, price, imageUrl } = req.body;
+
+  const course = await courseModel.findOne({
+    title: title,
+    description: description,
+    price: price,
+    imageUrl: imageUrl,
+  });
+
+  if (admin) {
+    const token = jwt.sign(
+      {
+        id: admin._id,
+      },
+      process.env.JWT_ADMIN_PASSWORD
+    );
+
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials",
+    });
+  }
+});
+
+module.exports = {
+  adminRouter,
+};
